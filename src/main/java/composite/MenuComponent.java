5611a7a7935143ddf187de81a2a2482f29afdd8f
Felipe Vidal <felipeisra.vidal@gmail.com>
2023-10-12 18:46:23 -0300

    implements the composite

---
+++ src/main/java/composite/MenuComponent.java
@@ -0,0 +1,8 @@
+package composite;
+
+public interface MenuComponent {
+    public void add(MenuComponent component);
+    public void remove(MenuComponent component);
+    public MenuComponent getChild(int index);
+    public void execute();
+}
